21f9dde425726e924aef8ecbebda0db0892109e4
Alik <alikhi@list.ru>
2025-08-22 13:41:25 +0300

    push commit #1

--- tests.py
+++ tests.py
@@ -4,21 +4,72 @@ from main import BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    def test_add_new_book_success(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
+        # добавляем книгу
+        collector.add_new_book('Гордость и предубеждение')
+        # проверяем, что добавилась книга
+        assert 'Гордость и предубеждение' in collector.books_genre
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_add_new_book_already_exists(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение')
+        collector.add_new_book('Гордость и предубеждение')
+        assert len(collector.books_genre) == 1
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_add_new_book_long_name(self):
+        collector = BooksCollector()
+        collector.add_new_book('Очень длинное название книги, которое превышает 40 символов')
+        assert 'Очень длинное название книги, которое превышает 40 символов' not in collector.books_genre
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_set_book_genre_success(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение')
+        collector.set_book_genre('Гордость и предубеждение', 'Комедии')
+        assert collector.get_book_genre('Гордость и предубеждение') == 'Комедии'
+
+    def test_set_book_genre_invalid_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение')
+        collector.set_book_genre('Гордость и предубеждение', 'Неизвестный жанр')
+        assert collector.get_book_genre('Гордость и предубеждение') == ''
+
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение')
+        collector.set_book_genre('Гордость и предубеждение', 'Комедии')
+        collector.add_new_book('1984')
+        collector.set_book_genre('1984', 'Фантастика')
+        assert collector.get_books_with_specific_genre('Комедии') == ['Гордость и предубеждение']
+
+    def test_get_books_for_children(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение')
+        collector.set_book_genre('Гордость и предубеждение', 'Комедии')
+        collector.add_new_book('1984')
+        collector.set_book_genre('1984', 'Фантастика')
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Оно', 'Ужасы')
+        assert collector.get_books_for_children() == ['Гордость и предубеждение', '1984']
+
+    def test_add_book_in_favorites_success(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение')
+        collector.add_book_in_favorites('Гордость и предубеждение')
+        assert 'Гордость и предубеждение' in collector.favorites
+
+    def test_delete_book_from_favorites_success(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение')
+        collector.add_book_in_favorites('Гордость и предубеждение')
+        collector.delete_book_from_favorites('Гордость и предубеждение')
+        assert 'Гордость и предубеждение' not in collector.favorites
+
+    def test_get_list_of_favorites_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение')
+        collector.add_book_in_favorites('Гордость и предубеждение')
+        collector.add_new_book('1984')
+        collector.add_book_in_favorites('1984')
+        assert collector.get_list_of_favorites_books() == ['Гордость и предубеждение', '1984']   
